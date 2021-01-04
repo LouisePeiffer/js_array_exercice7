07cf3af0129cdb30e6c7ba2ab381c3e0105249aa
Louise <louisepeiffer@hotmail.com>
2021-01-04 14:17:40 +0100

    exos

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+// Exo 1
+let prenoms = ['Adame', 'Béné', 'Ayhan', 'Louise']
+console.log(prenoms);
+console.log(prenoms[3]);
+console.log(prenoms.length);
+
+// Exo 2
+let monPrenom = ['Louise']
+monPrenom.push('Peiffer')
+console.log(monPrenom);
+
+// Exo 3
+let coding16 = ['Ayhan']
+coding16.push('Béné')
+coding16.push('Louise', 'Jamila')
+console.log(coding16);
+coding16.push('Sebastien', 'Adame', 'Nicolas')
+coding16.push('Dawid', 'Malo')
+console.log(coding16);
+coding16.splice(6, 1)
+console.log(coding16);
